fa2a6f7a93d3b35a47bcc4b2ffad2a0f295b4827
cerendogann <cerendogan02@gmail.com>
2025-12-30 14:41:51 +0300

    Initialize gui package for UI component management

---
+++ PC_Controller/gui/__init__.py
@@ -0,0 +1,9 @@
+"""
+Home Automation System GUI Module
+Provides modern CustomTkinter interface for the home automation system
+"""
+
+from .main_window import SmartHomeApp
+
+__all__ = ['SmartHomeApp']
+
