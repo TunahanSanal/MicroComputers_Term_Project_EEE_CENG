b6ab21b8ef64e464e6004ddb52d2e3cf4fc5548f
cerendogann <cerendogan02@gmail.com>
2025-12-30 14:39:34 +0300

    Initialize api package for modular access

---
+++ PC_Controller/api/__init__.py
@@ -0,0 +1,15 @@
+"""
+Home Automation System API Module
+Provides UART communication with PIC16F877A microcontrollers
+"""
+
+from .base_connection import HomeAutomationSystemConnection
+from .air_conditioner import AirConditionerSystemConnection
+from .curtain_control import CurtainControlSystemConnection
+
+__all__ = [
+    'HomeAutomationSystemConnection',
+    'AirConditionerSystemConnection',
+    'CurtainControlSystemConnection'
+]
+
